5573142df4755b989e27531de9e294ff7f19188e
Scott Bettigole <scott.bettigole@tufts.edu>
2020-12-05 20:44:57 -0500

    .

--- api/MoodMatcher/__init__.py
+++ api/MoodMatcher/__init__.py
@@ -1,12 +1,14 @@
+import os
+
 from flask import Flask
 
-class heroku_config(Object):
+class HerokuConfig:
     SPOT_CLIENT_ID = os.environ['SPOT_CLIENT_ID']
     SPOT_CLIENT_SECRET = os.environ['SPOT_CLIENT_SECRET']
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(heroku_config)
+    app.config.from_object(HerokuConfig)
 
     from MoodMatcher import routes
 
