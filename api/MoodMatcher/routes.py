2a3864f6f7620023e8c34be5a8b923410ca6d199
Scott Bettigole <scott.bettigole@tufts.edu>
2020-12-05 20:47:54 -0500

    switch to current_app

--- api/MoodMatcher/routes.py
+++ api/MoodMatcher/routes.py
@@ -1,26 +1,26 @@
-from flask import jsonify
+from flask import jsonify, current_app
 
-from MoodMatcher import app
+# from MoodMatcher import app
 from spotify_scraping.category_parser import get_track
 from spotify_scraping.category_parser import get_playlists
 from spotify_scraping.data_storage import read_audio_features_for_category
 
-@app.route('/')
+@current_app.route('/')
 def index():
     return "Hello, World!"
 
 
-@app.route('/<category>/get_playlists')
+@current_app.route('/<category>/get_playlists')
 def category_playlists(category):
     return jsonify(get_playlists(category))
 
 
-@app.route('/<category>/category_track_feats')
+@current_app.route('/<category>/category_track_feats')
 def cat_track_features(category):
     return read_audio_features_for_category(category).to_html(header=True)
 
 
-@app.route('/<track>/info')
+@current_app.route('/<track>/info')
 def get_track_info(track):
     track_info = get_track(track)
     if track_info.get('error'):
