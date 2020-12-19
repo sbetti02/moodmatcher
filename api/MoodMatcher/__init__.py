5a76f8e6ac874abb2e4bd7f8f4799a9bebb91c9e
Scott Bettigole <scott.bettigole@tufts.edu>
2020-12-19 16:37:41 -0500

    Deploy client (#6)
    * Set up base url for api
    
    * change var name
    
    * Fix cors issue

--- api/MoodMatcher/__init__.py
+++ api/MoodMatcher/__init__.py
@@ -1,8 +1,10 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SPOT_CLIENT_ID'] = os.environ['SPOT_CLIENT_ID']
 app.config['SPOT_CLIENT_SECRET'] = os.environ['SPOT_CLIENT_SECRET']
 app.config['PYTHONUNBUFFERED'] = True
---
+++ client/src/util.js
@@ -0,0 +1,1 @@
+export const API_BASE = process.env.REACT_APP_API_BASE;
